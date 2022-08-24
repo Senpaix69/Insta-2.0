18a3df89cf28743a6b9118b7dbe1ba5b1a8a5cde
Senpai <raohuraira331.rb@gmail.com>
2022-08-25 03:49:55 +0500

    fixed bugs

--- components/Chat.js
+++ components/Chat.js
@@ -22,14 +22,12 @@ const Chat = ({ username, userImg, timeStamp, id, setActiveChatID }) => {
     ]);
     const [text, setText] = useState('');
 
-
-
     return (
         <div className="max-w-6xl lg:mx-auto flex justify-center bg-gray-100">
-            <div className="bg-[url('https://i.pinimg.com/originals/b7/fc/af/b7fcaf2631fc54f28ef3f123855d03dc.jpg')] bg-no-repeat bg-cover bg-center w-full h-screen flex flex-col md:w-[700px]">
+            <div className="bg-[url('https://i.pinimg.com/originals/b7/fc/af/b7fcaf2631fc54f28ef3f123855d03dc.jpg')] bg-no-repeat bg-cover bg-center w-full flex flex-col md:w-[700px] h-screen overflow-y-scroll scrollbar-hide">
 
                 {/* Chat Header */}
-                <section className="shadow-md border-b bg-transparent sticky top-0 z-50">
+                <section className="shadow-sm bg-white sticky top-0 z-50">
                     <div className="flex items-center px-2 py-1">
                         <ArrowLeftIcon onClick={() => setActiveChatID(-1)} className="btn ml-1" />
                         <img
@@ -51,8 +49,8 @@ const Chat = ({ username, userImg, timeStamp, id, setActiveChatID }) => {
                     {msgs?.map(msg => (
                         <div key={msg.userid} className={`flex mt-1 ${msg.userid === session?.user.uid ? "justify-end" : ""}`}>
                             <div className="flex items-center rounded-md w-fit max-w-xs py-1 px-2 relative">
-                                <img src={msg.userImg} alt='Profile' className={`h-7 w-7 md:h-8 md:w-8 rounded-full cursor-pointer absolute top-1 ${msg.userid === session?.user.uid ? "right-2" : ""}`} />
-                                <p className={`${msg.userid === session?.user.uid ? "mr-10" : "ml-10"} bg-gray-300 p-2 rounded-lg`}>{msg.text}
+                                <img src={msg.userImg} alt='Profile' className={`h-8 w-8 rounded-full cursor-pointer absolute top-1 ${msg.userid === session?.user.uid ? "right-2" : ""}`} />
+                                <p className={`${msg.userid === session?.user.uid ? "mr-9" : "ml-9"} bg-gray-300 p-2 rounded-lg`}>{msg.text}
                                     <Moment fromNow className="ml-2 text-[10px] text-gray-500">
                                         <span>{msg.timeStamp}</span>
                                     </Moment>
@@ -80,7 +78,9 @@ const Chat = ({ username, userImg, timeStamp, id, setActiveChatID }) => {
                         <button
                             type="submit"
                             disabled={text ? false : true}
-                            className="mr-3 font-semibold text-sm text-blue-500 disabled:text-gray-400">send</button>
+                            className="mr-3 font-semibold text-sm text-blue-500 disabled:text-gray-400">
+                            send
+                        </button>
                     </div>
                 </section>
             </div>
