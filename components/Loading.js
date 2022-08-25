5204d8d2e210ef5eb88f4e19a3587e0685e2b7ab
Senpai <raohuraira331.rb@gmail.com>
2022-08-25 15:40:24 +0500

    Loading Fix

--- components/Loading.js
+++ components/Loading.js
@@ -2,7 +2,7 @@ import { ThreeDots } from 'react-loader-spinner';
 
 const Loading = () => {
     return (
-        <div className='flex items-center w-full h-screen justify-center'>
+        <div className='flex items-center w-full mt-56 justify-center'>
             <ThreeDots color='#00BFFF' height={50} width={40} />
         </div>
     )
