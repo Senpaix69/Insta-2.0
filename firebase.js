2588335c5a37e3cf4d6fbfdd24fbddce9f7ae7f4
Senpai <raohuraira331.rb@gmail.com>
2022-08-26 03:13:31 +0500

    modied

--- firebase.js
+++ firebase.js
@@ -2,11 +2,8 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { getAuth } from 'firebase/auth';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyB2JUoj6AT0T5S0VyF70DgcNjN296HzjnA",
   authDomain: "insta-by-senpai.firebaseapp.com",
@@ -20,5 +17,6 @@ const firebaseConfig = {
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore();
 const storage = getStorage();
+const auth = getAuth();
 
-export { app, db, storage };
+export { app, db, storage, auth };
