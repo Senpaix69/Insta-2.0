b55b3577324792467d0f2cc102039961a43af1c3
Senpai <raohuraira331.rb@gmail.com>
2022-08-27 14:46:12 +0500

    checking notifications

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
   return (
     <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
       <Head>
-        <title>Instagram By Senpai</title>
+        <title>Insta-2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
