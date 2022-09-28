1afc58c3f1c555e72f061c7a55cc820294e78fa1
Senpai <raohuraira331.rb@gmail.com>
2022-09-29 04:05:09 +0500

    testing server

--- pages/index.js
+++ pages/index.js
@@ -70,6 +70,10 @@ export default function Home() {
     }
   }, [load]);
 
+  const sendNotification = () => {
+    initBeams("Push");
+  };
+
   return (
     <div
       className={`h-screen overflow-y-scroll scrollbar-hide ${
@@ -80,6 +84,9 @@ export default function Home() {
         <title>InstaPro</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <button className="h-10 w-full text-white" onClick={sendNotification}>
+        Send Notification
+      </button>
       {!loading && (
         <Header darkMode={darkMode} setDarkMode={setDarkMode} user={user} />
       )}
