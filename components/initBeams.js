f8785f22328b5aca0c0ed9e6eeb4984f2d9d8fbd
Senpai <raohuraira331.rb@gmail.com>
2022-10-02 15:36:33 +0500

    added notifications

--- components/initBeams.js
+++ components/initBeams.js
@@ -1,16 +1,10 @@
 import * as PusherPushNotifications from "@pusher/push-notifications-web";
-import PushNotifications from "@pusher/push-notifications-server";
 import { toast } from "react-toastify";
 
 const initBeams = (uid, signOut) => {
   const beamsClient = new PusherPushNotifications.Client({
     instanceId: "e7843789-8f6e-4c23-86d2-14faddde20fe",
   });
-  // const pushNotifications = new PushNotifications({
-  //   instanceId: "e7843789-8f6e-4c23-86d2-14faddde20fe",
-  //   secretKey:
-  //     "971CDA3FEF7ED98D8A949F7441BD30735EB6DB57254A8F5BE917A48CB6129243",
-  // });
   beamsClient
     .getRegistrationState()
     .then((state) => {
@@ -53,23 +47,6 @@ const initBeams = (uid, signOut) => {
                 toast.error(`Error: ${e}`, { position: "top-center" });
                 console.error("Could not clear Beams state", e);
               });
-          } else if (uid === "Push") {
-            // console.log("Sending");
-            // pushNotifications
-            //   .publishToInterests(["debug-100490208109736352301"], {
-            //     web: {
-            //       notification: {
-            //         title: "Senpai",
-            //         body: "Hello, Boy!",
-            //       },
-            //     },
-            //   })
-            //   .then((publishResponse) => {
-            //     console.log("Just published:", publishResponse.publishId);
-            //   })
-            //   .catch((error) => {
-            //     console.log("Error:", error);
-            //   });
           }
           break;
         }
@@ -77,6 +54,8 @@ const initBeams = (uid, signOut) => {
         case states.PERMISSION_PROMPT_REQUIRED: {
           beamsClient
             .start()
+            .then(() => beamsClient.addDeviceInterest(uid))
+            .then(() => beamsClient.addDeviceInterest("public"))
             .then(() => beamsClient.addDeviceInterest(`debug-${uid}`))
             .then(() => beamsClient.addDeviceInterest("debug-public"))
             .then(() => {
